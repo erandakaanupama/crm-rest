ed5424c2d7544f0c47e6f59f79969682ae807e63
erandakaanupama <erandakaanupama@gmail.com>
2018-07-26 16:58:45 +0530

    add new customer

--- src/main/java/com/eab/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/eab/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,27 +16,36 @@ import com.eab.springdemo.service.CustomerService;
 @RestController
 @RequestMapping("/api")
 public class CustomerRestController {
-	
+
 	// autowire customer service
 	@Autowired
 	private CustomerService customerService;
-	
+
 	// add mapping for GET /customers
 	@GetMapping("/customers")
-	public List<Customer> getCustomers(){
-		
+	public List<Customer> getCustomers() {
+
 		return customerService.getCustomers();
 	}
-	
+
 	// add mapping fot GET /customers/{customerID}
 	@GetMapping("/customers/{customerId}")
 	public Customer getCustomer(@PathVariable int customerId) {
-		
+
 		Customer theCustomer = customerService.getCustomer(customerId);
-		
+
 		if (theCustomer == null) {
 			throw new CustomerNotFoundException("Customer id not found - " + customerId);
 		}
+		return theCustomer;
+	}
+
+	//
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+
+		theCustomer.setId(0);
+		this.customerService.saveCustomer(theCustomer);
 		return theCustomer;
 	}
 
