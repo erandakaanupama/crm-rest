79a3ff0b1cb744faf42d741120ce43250ca56d37
erandakaanupama <erandakaanupama@gmail.com>
2018-07-26 17:19:27 +0530

    update customer

--- src/main/java/com/eab/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/eab/springdemo/rest/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,11 +41,19 @@ public class CustomerRestController {
 		return theCustomer;
 	}
 
+	// add new customer
 	//
 	@PostMapping("/customers")
 	public Customer addCustomer(@RequestBody Customer theCustomer) {
 
 		theCustomer.setId(0);
+		this.customerService.saveCustomer(theCustomer);
+		return theCustomer;
+	}
+	
+	// update a customer
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
 		this.customerService.saveCustomer(theCustomer);
 		return theCustomer;
 	}
